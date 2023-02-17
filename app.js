724f4a63196808621410291e4625a99da7eb7cae
Nul0 <danielpedro7676@gmail.com>
2023-02-17 00:23:40 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const navBar = document.querySelector('nav');
+const arrowDown = document.querySelector(".arrowDown");
+
+// Função para verificar quando tem scroll na tela
+const verify_scrolled = () => {
+    navBar.classList.toggle('scrolled_screen', scrollY > 0);
+    arrowDown.classList.toggle('scrolled_screen', scrollY > 0);
+}
+
+window.addEventListener("scroll", verify_scrolled);
+
+//Animação do gato com lottie
+let animation_div = document.querySelector(".cat_coffe_animation");
+let animation_object = bodymovin.loadAnimation({
+    container: animation_div,
+    renderer: 'svg',
+    loop: true,
+    autoLoadSegments: false,
+    path: "https://assets5.lottiefiles.com/packages/lf20_xkbhgbld.json"
+})
